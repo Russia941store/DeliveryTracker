7f37810c8d75862a5247fb1c9f7f2dc297c1701f
Nickay Fake <nickay_fake@MacBook-Air-Nickay.local>
2025-04-24 17:09:33 +0500

    🔧 fix: подключение переменных окружения и переход на вебхук

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN_2")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID_2"))
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# main.py
-
+#main.py
 import asyncio
 from config import BOT_TOKEN, WEBHOOK_URL
+from handlers import register_handlers
 from telegram.ext import ApplicationBuilder
 
 async def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-    # app.add_handler(...) ← Добавь нужные обработчики сюда
+    register_handlers(app)
     await app.initialize()
     await app.start()
-    await app.bot.set_webhook(url=f"{WEBHOOK_URL}/webhook")
+    await app.bot.set_webhook(WEBHOOK_URL)
     print("Бот запущен через вебхук...")
-    await asyncio.Event().wait()
+    await app.updater.start_polling()  # может быть убран, если не нужен
 
 if __name__ == "__main__":
     asyncio.run(main())
