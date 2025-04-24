8871f8ea8ad0e2e1ddf1be5c119b39738bcc4e1a
Nickay Fake <nickay_fake@MacBook-Air-Nickay.local>
2025-04-24 18:03:29 +0500

    fix: добавлен app.initialize() для запуска через webhook

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 # main.py
 import asyncio
-import logging
 from config import BOT_TOKEN, WEBHOOK_URL
 from handlers import register_handlers
 from telegram.ext import ApplicationBuilder
-
-logging.basicConfig(level=logging.INFO)
 
 async def main():
     print(">>> Инициализация приложения")
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     register_handlers(app)
-    print(f">>> Установка вебхука: {WEBHOOK_URL}")
+
+    print(">>> Установка вебхука:", WEBHOOK_URL)
     await app.bot.set_webhook(WEBHOOK_URL)
+
     print(">>> Запуск приложения через webhook...")
+    await app.initialize()
     await app.start()
     print(">>> Готово. Ждём события...")
+    await app.updater.wait()
 
 if __name__ == "__main__":
     asyncio.run(main())
