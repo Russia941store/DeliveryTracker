008f7136e63bdc560e773e6a416e6e3f1f5039f5
Nickay Fake <nickay_fake@MacBook-Air-Nickay.local>
2025-04-24 16:36:56 +0500

    🚀 force push: обновление всего проекта

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 # main.py
+import asyncio
+from telegram.ext import Application, ApplicationBuilder, CommandHandler
+from handlers import start_handler
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
-from telegram.ext import ApplicationBuilder
-from handlers import get_conv_handler
-from config import BOT_TOKEN
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # берем токен из переменной окружения
+WEBHOOK_PATH = f"/{BOT_TOKEN}"  # по этому пути Telegram будет отправлять сообщения
+PORT = int(os.environ.get("PORT", 8443))  # стандартный порт
+APP_URL = os.environ.get("WEBHOOK_URL")  # твой публичный адрес на Amvera
 
 async def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(get_conv_handler())
-    
+
+    app.add_handler(CommandHandler("start", start_handler))
+
     await app.initialize()
     await app.start()
-    print("Бот запущен...")
-    await app.updater.start_polling()
-
-# === запуск без закрытия event loop ===
-import asyncio
+    await app.bot.set_webhook(url=f"{APP_URL}{WEBHOOK_PATH}")  # Регистрируем webhook в Telegram
+    await app.updater.start_webhook(
+        listen="0.0.0.0",        # слушаем на всех интерфейсах
+        port=PORT,              # порт (стандартный 8443)
+        webhook_path=WEBHOOK_PATH,  # путь, куда прилетают сообщения от Telegram
+    )
+    await app.updater.wait_until_closed()  # ждем, пока не остановят
 
 if __name__ == "__main__":
-    try:
-        loop = asyncio.get_event_loop()
-        loop.create_task(main())
-        loop.run_forever()
-    except (KeyboardInterrupt, SystemExit):
-        pass
+    asyncio.run(main())
