35884cbc1f50ea8aec2ac9ac358421af773fc3c6
Nickay Fake <nickay_fake@MacBook-Air-Nickay.local>
2025-04-24 17:48:50 +0500

    🔧 Обновлен main.py для работы через webhook

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-#main.py
+# main.py
 import asyncio
-import logging
 from config import BOT_TOKEN, WEBHOOK_URL
 from handlers import register_handlers
 from telegram.ext import ApplicationBuilder
 
-logging.basicConfig(level=logging.INFO)
-
 async def main():
-    try:
-        print(">>> Инициализация приложения")
-        app = ApplicationBuilder().token(BOT_TOKEN).build()
-        register_handlers(app)
-        await app.initialize()
-        print(f">>> Установка вебхука: {WEBHOOK_URL}")
-        await app.bot.set_webhook(WEBHOOK_URL)
-        print(">>> Запуск приложения")
-        await app.start()
-        print(">>> Готово. Ждём события...")
-        await app.updater.start_polling()  # добавим polling для живости, если webhook не работает
-    except Exception as e:
-        print(">>> Ошибка при запуске:", e)
+    print(">>> Инициализация приложения")
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    register_handlers(app)
+    print(f">>> Установка вебхука: {WEBHOOK_URL}")
+    await app.initialize()
+    await app.bot.set_webhook(WEBHOOK_URL)
+    print(">>> Запуск приложения через webhook...")
+    await app.start()
+    print(">>> Готово. Ждём события...")
+    await app.updater.start()  # запуск цикла webhook listener
+    await app.updater.wait_until_shutdown()  # блокирует выход из приложения
 
 if __name__ == "__main__":
     asyncio.run(main())
