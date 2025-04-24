5fe9cc53e66185a193c7493ecf615d883ddb95ec
Nickay Fake <nickay_fake@MacBook-Air-Nickay.local>
2025-04-24 17:56:35 +0500

    ✅ Fix webhook run: remove invalid updater.start()

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 # main.py
 import asyncio
+import logging
 from config import BOT_TOKEN, WEBHOOK_URL
 from handlers import register_handlers
 from telegram.ext import ApplicationBuilder
+
+logging.basicConfig(level=logging.INFO)
 
 async def main():
     print(">>> Инициализация приложения")
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     register_handlers(app)
     print(f">>> Установка вебхука: {WEBHOOK_URL}")
-    await app.initialize()
     await app.bot.set_webhook(WEBHOOK_URL)
     print(">>> Запуск приложения через webhook...")
     await app.start()
     print(">>> Готово. Ждём события...")
-    await app.updater.start()  # запуск цикла webhook listener
-    await app.updater.wait_until_shutdown()  # блокирует выход из приложения
 
 if __name__ == "__main__":
     asyncio.run(main())
